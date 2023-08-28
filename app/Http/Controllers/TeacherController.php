d346ef4a44ff29d939f7e4c98582518287545be9
Alvarobyui <u701860138@byui.edu>
2023-08-28 02:25:38 -0500

    maestros, done

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Teacher;
+use Illuminate\Validation\ValidationException;
 
 class TeacherController extends Controller
 {
@@ -11,6 +12,7 @@ class TeacherController extends Controller
         $teachers = Teacher::where('status', 1)->get();
         return $teachers;
     }
+
     public function getByID($id) {
         $teacher = Teacher::where('id', $id)->where('status', 1)->first();
         if(!$teacher) {        
@@ -18,17 +20,30 @@ class TeacherController extends Controller
         }
         return $teacher;
     }
+
     public function create(Request $request) {
-        $teacher = new Teacher();
-        $teacher->name = $request->name;
-        $teacher->role = 2;
-        $teacher->status = 1;
-        $teacher->email = $request->email;
-        $teacher->password = $request->password;
-        $teacher->save();
+        try {
+            $validateData = $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|email|unique:teachers,email',
+                'password' => 'required|string|min:8',
+            ]);
         
-        return $teacher;
+
+            $teacher = new Teacher();
+            $teacher->name = $validateData['name'];
+            $teacher->role = 2;
+            $teacher->status = 1;
+            $teacher->email = $validateData['email'];
+            $teacher->password = bcrypt($validateData['password']);
+            $teacher->save();
+            
+            return $teacher;
+        } catch (ValidationException $e) {
+            return response()->json(['error' => $e->errors()], 422);
+        }
     }
+
     public function update(Request $request, Teacher $teacher) {
         if($teacher->status != 1) {
             return response()->json(['error'=>'Teacher not found or deleted. Please, choose another one.']);
